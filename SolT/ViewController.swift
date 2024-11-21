087db8171620f9939892a47a71dc3dafe85dfe09
Leo <dev@tjlabscorp.com>
2024-11-21 16:41:15 +0900

    Initial Commit

---
+++ SolT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolT
+//
+//  Created by 신동현 on 11/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
